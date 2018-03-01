af729d4b1a4ca0be544f2a3f43cd8d55ab0c11e1
markpeou <markpeou@gmail.com>
2018-03-02 10:47:20 +1100

    set background image. it works, no stretchyness. excellent.

--- app/components/App.js
+++ app/components/App.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import RecipeList from './RecipeList'
 import RecipeInfo from './RecipeInfo'
-import Background from './background3.jpg'
-import logo from  './logo.png'
+import background from './background3.jpg'
 
 export default class App extends React.Component {
   constructor(props) {
@@ -27,20 +26,34 @@ export default class App extends React.Component {
     this.setState({recipes: 'loading recipes...'})
     let {input1, input2, input3} = this.state
     var inputs = `${input1}+${input2}+${input3}`
+    if (input1 == '' || input2 == '' || input3 == ''){
+      this.setState({recipes:'need more ingredients...'})
+    } else {
       fetch(`https://api.edamam.com/search?q=${inputs}&app_id=0296d6c7&app_key=622b6a9f8811b21a64fa77f17cb3c4e8`)
       .then(res => res.json())
       .then(res => this.setState({recipes: res.hits}))
     }
+  }
+
 
   componentDidMount() {
     console.log('olio')
   }
 
   render() {
+    const style_1 = {
+      padding: '180px 0',
+      backgroundImage: `url(${background})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center center no-repeat',
+      backgroundRepeat: 'no-repeat',
+    }
 
     return <div>
 
-      <img className='background' src={Background} />
+    <section className="page-background" style={style_1} data-stellar-background-ratio="0.4">
+    </section>
+
       <div className='form'>
       <p>list 3 ingredients you have and we will find a recipe to match your ingredients!</p>
       <input className='input1' type='text' onChange={this.getInput} />
